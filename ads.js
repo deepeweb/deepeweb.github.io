02c0435b9b37f09d336fd64a97b4902a27e0ac9a
Deepanshu Shakya <priyanshugamer1080@gmail.com>
2025-07-11 19:36:10 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,17 +1,17 @@
 (function(){
   var ads = [
     {
-      img: "https://deepstudyweb.github.io/assets/insta-add.gif",
+      img: "https://deepeweb.github.io/assets/insta-add.gif",
       url: "https://www.instagram.com/deepanshuranjan80?igsh=dHhmOGR0cmZxaGRi",
       text: "Follow Me!"
     },
     {
-      img: "https://deepstudyweb.github.io/assets/fb-pic-deepanshu.jpg",
+      img: "https://deepeweb.github.io/assets/fb-pic-deepanshu.jpg",
       url: "https://www.facebook.com/share/19MEJCU9fj/",
       text: "🔥 Deal 2 - Limited Time!"
     },
     {
-       img: "https://deepstudyweb.github.io/assets/insta-dp.jpg",
+       img: "https://deepeweb.github.io/assets/insta-dp.jpg",
       url: "https://www.facebook.com/share/19MEJCU9fj/",
       text: "🔥 Deal 2 - Limited Time!"
     }
